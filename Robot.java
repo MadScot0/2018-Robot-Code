99e416513fad1a2bb831e3fdd6a0b04b7c8d04a3
MadScot0 <35280184+MadScot0@users.noreply.github.com>
2018-01-13 12:29:45 -0800

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3826.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 //import org.opencv.core.Rect;
@@ -28,7 +30,13 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.vision.VisionThread;
-//                                          2018 Bot Code//
+//                                2018 Bot Code// v1.1
+			/* Implemented turning PID
+			 * Untested
+			 * 
+			 * 
+			 * 
+			 */
 /**
  *
  * The VM is configured to automatically run this class, and to call the
@@ -48,11 +56,18 @@ public class Robot extends IterativeRobot {
 	Talon frontLeft = new Talon(0);
 	Talon rearLeft = new Talon(1); 
 	Talon frontRight = new Talon(2); 
-	Talon rearRight = new Talon(3); 
+	Talon rearRight = new Talon(3);
+	
+	SRF_PID turningPID;
+	
+	AHRS ahrs;
+	double angleNow, scaledAngle; //the current angle and the angle to feed into the PID
+	
+	Timer t = new Timer();
 	
     public void robotInit() {
     	robot = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
-    	
+    	turningPID = new SRF_PID();
     	xBox = new Joystick(0);
     }
     
@@ -60,7 +75,10 @@ public class Robot extends IterativeRobot {
      * This function is run once each time the robot enters autonomous mode
      */
     public void autonomousInit() {
-    	
+    	turningPID.setPID(3, 0.2, 0.1);
+    	turningPID.setLimits(1, -1);
+    	turningPID.setSetpoint(0.25);
+    	t.start();
     }
     
     /**
@@ -68,6 +86,7 @@ public class Robot extends IterativeRobot {
      */
 	public void autonomousPeriodic() {
 		
+		robot.arcadeDrive(0,turningPID.computePID(ahrs.getAngle()/360, t.get()));
     }
     	
     
